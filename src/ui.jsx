e677d652fad2eb9cd9cc5c75c2143e1a9f9c3b68
losta <lolontsinde@gmail.com>
2022-08-15 17:10:41 +0200

    Update ui.jsx

--- src/ui.jsx
+++ src/ui.jsx
@@ -76,7 +76,7 @@ export const IdTokenClaims = (props) => {
             <p><strong>Audience: </strong> {props.idTokenClaims.aud}</p>
             <p><strong>Issuer: </strong> {props.idTokenClaims.iss}</p>
             <p><strong>OID: </strong> {props.idTokenClaims.oid}</p>
-            <p><strong>UPN: </strong> {props.idTokenClaims.preferred_username}</p>
+            <p><strong>UPN: </strong> {props.idTokenClaims.given_name}</p>
         </div>
     );
 }
